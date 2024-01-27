c372e27df19cb5ae640c3ad42875615b5322baaa
Fagner Valente <fagnervalente@gmail.com>
2024-01-27 02:09:13 -0300

    Test postgres ssl true

--- src/adapter/database/data-source.ts
+++ src/adapter/database/data-source.ts
@@ -4,9 +4,7 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  ssl:  {
-    rejectUnauthorized: false
-  },
+  ssl: true,
   host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT as number | undefined,
   username: process.env.DATABASE_USER,
