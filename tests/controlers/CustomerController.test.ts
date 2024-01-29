3f3264f60776bae0d41fc4ba5eeeb12c2d95ef6e
Fagner Valente <fagnervalente@gmail.com>
2024-01-29 19:33:16 -0300

    Add more testes

--- tests/controlers/CustomerController.test.ts
+++ tests/controlers/CustomerController.test.ts
@@ -1,20 +1,48 @@
-import { describe, test, expect } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
 import { CustomerController } from '../../src/controllers/CustomerController';
+import MockCustomerRepository from '../mocks/MockCustomerRepository';
 
-test("Creating Customer entity", () => {
+describe("CustomerController", () => {
+
+    let customerController: CustomerController;
+    const mockCustomerRepository = new MockCustomerRepository();
+
+    const customerOne = {
+        name: "example",
+        cpf: "11111111111",
+        email: "example@test.com"
+    }
+
+    beforeEach(() => {
+        customerController = new CustomerController(mockCustomerRepository)
+    });
+
+    describe("constructor", () => {
+        it("Set repository", () => {
+            expect((customerController as any).repository).not.toBeUndefined();
+        })
+    })
+
     describe("create", () => {
-
+        it("Should create customer", async () => {
+            const created = await customerController.create(customerOne.name, customerOne.cpf, customerOne.email);
+            expect(created).toMatchObject(customerOne);
+        })
+        it("Should throw an error by using a wrong email", async () => {
+            await expect(() => customerController.create('', '', '')).rejects.toThrowError();
+        })
     })
 
     describe("list", () => {
-
+        it("Must return null", async () => {
+            const result = await customerController.list();
+            expect(result).toBeNull();
+        })
     })
 
     describe("getCustomerByCPF", () => {
-
-    })
-
-    describe("delete", () => {
-
+        it("Should return an error", async () => {
+            await expect(() => customerController.getCustomerByCPF('')).rejects.toThrowError();
+        })
     })
 })
